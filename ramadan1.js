89a80720fefceb2a7862ae7ae124cf4bba5bb68a
mr-fhd <58129426+mr-fhd@users.noreply.github.com>
2022-03-20 19:02:32 +0300

    Update ramadan1.js

--- ramadan1.js
+++ ramadan1.js
@@ -4,3 +4,4 @@ setTimeout(function () {
 spinner.style.display = "none"; document.body.style.overflow = "auto"; 
 }, 6000);
 }
+$(`${'<img src="https://i.top4top.io/p_1934bx3ie1.png" class=".rainbow" style="position: absolute;transform: matrix(1, 0, 0, 1, 0, 60);right: 8px;background-color: #ffffff00;width:45px;top: -57px;z-index: 2;"><img src="https://j.top4top.io/p_19342fguv2.png" class=".rainbow" style="position: absolute;transform: matrix(1, 0, 0, 1, 0, 60);right: 0px;background-color: #ffffff00;width: 48px;top: 0px;z-index: 1;">'}`)['insertBefore']('.rainbow');
